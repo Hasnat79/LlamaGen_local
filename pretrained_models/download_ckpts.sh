afc10964a6c321c81a36a36a8625a715b4ddc107
Hasnat79 <hasnatabdullah79@gmail.com>
2024-10-26 01:47:58 -0500

    ckpt download script

---
+++ pretrained_models/download_ckpts.sh
@@ -0,0 +1,4 @@
+git clone https://huggingface.co/google/flan-t5-xl
+wget https://huggingface.co/peizesun/llamagen_t2i/resolve/main/t2i_XL_stage1_256.pt
+wget https://huggingface.co/peizesun/llamagen_t2i/resolve/main/vq_ds16_t2i.pt
+
